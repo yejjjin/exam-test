b9714242e9ad0c4ff76d58c1c600c7eb99e491a8
yejjjin <ggreat7712@gmail.com>
2024-03-04 15:22:49 +0900

    능력단위평가 철길마을

---
+++ js/comm.js
@@ -0,0 +1,20 @@
+$(function () {
+  let now = 0;
+
+  const slideCount = $(".slide ul li").length;
+  const slideList = $(".slide ul");
+
+  setInterval(function () {
+    now = (now + 1) % slideCount;
+    slideList.css("left", 100 * -now + "%");
+  }, 3000);
+
+  //   modal
+  $(".list-bbs li:first-child").on("click", function () {
+    $("#modal").css("display", "flex");
+  });
+
+  $(".modal-close").on("click", function () {
+    $("#modal").css("display", "none");
+  });
+});
